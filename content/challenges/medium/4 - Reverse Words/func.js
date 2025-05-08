9765d7402ddf999f71664ca17b623788ae404736
cpleasants <chris.pleasants@generalassemb.ly>
2025-05-07 21:37:55 -0400

    did 4 with .reverse()

--- content/challenges/medium/4 - Reverse Words/func.js
+++ content/challenges/medium/4 - Reverse Words/func.js
@@ -11,20 +11,15 @@
  * @returns {string} The string with the proper words reversed
  */
 module.exports = (str) => {
-    words = str.split(" ")
-    let rev_words = []
+    let words = str.split(" ");
+    let rev_words = [];
     for (i = 0; i < words.length; i++) {
         if (words[i].length >= 5) {
-            let word = words[i]
-            let rev_word = ''
-            for (j = word.length - 1; j >= 0; j--) {
-                rev_word += word[j]
-            }
-            rev_words.push(rev_word)
+            let rev_word = words[i].split('').reverse().join('');
+            rev_words.push(rev_word);
         } else {
-            rev_words.push(words[i])
+            rev_words.push(words[i]);
         }
-       
     }
     return rev_words.join(" ")
 }
