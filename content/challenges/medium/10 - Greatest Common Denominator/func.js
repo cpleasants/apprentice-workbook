61b824e621384a8f9cfe2df9dcdd9e2c565d1a75
cpleasants <chris.pleasants@generalassemb.ly>
2025-05-07 20:47:14 -0400

    added short circuit to problem 10

--- content/challenges/medium/10 - Greatest Common Denominator/func.js
+++ content/challenges/medium/10 - Greatest Common Denominator/func.js
@@ -11,29 +11,28 @@
  * @returns {number} GCD
  */
 module.exports = (int1, int2) => {
-    let greater;
-    let lesser;
-    if (int1 > int2) {
-        greater = int1;
-        lesser = int2;
-    } else {
-        greater = int2;
-        lesser = int1;
-    }
-    // function gcd(greater, lesser) {
-    //     let remainder = greater % lesser;
-    //     if (remainder === 0) {
-    //         return lesser
-    //     } else {
-    //         return gcd(lesser, remainder)
-    //     }
-    // }
-    // return gcd(greater, lesser)
-    let remainder = greater % lesser
-    while (remainder !== 0) {
-        greater = lesser
-        lesser = remainder
-        remainder = greater % lesser
-    }
-    return lesser
+  
+  if (int1 % int2 === 0) {
+    return int2;
+  } else if (int2 % int1 === 0) {
+    return int1;
+  }
+  
+  let greater;
+  let lesser;
+  if (int1 > int2) {
+      greater = int1;
+      lesser = int2;
+  } else {
+      greater = int2;
+      lesser = int1;
+  }
+
+  let remainder = greater % lesser;
+  while (remainder !== 0) {
+      greater = lesser;
+      lesser = remainder;
+      remainder = greater % lesser;
+  }
+  return lesser
 }
